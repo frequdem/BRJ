051fd759606e83cd24958d9a3b9f810a42b1146e
xunke <frequdem@163.com>
2016-11-20 23:58:05 +0800

    list continue

--- public/javascripts/js/list/list.js
+++ public/javascripts/js/list/list.js
@@ -48,6 +48,14 @@
 		$('span.login-img').click(function () {
 			location.href = '/login';
 		});
+		$('.fy-item-cover .like-icon').click(function () {
+			var _thisJq = $(this);
+			if (_thisJq.hasClass('do-like')) {
+				_thisJq.removeClass('do-like').addClass('donot-like');
+			} else if (_thisJq.hasClass('donot-like')) {
+				_thisJq.removeClass('donot-like').addClass('do-like');
+			}
+		});
 	});
 
 /***/ }
--- public/javascripts/jsentry/list/list.js
+++ public/javascripts/jsentry/list/list.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 	$('span.login-img').click(function() {
 		location.href = '/login';
+	});
+	$('.fy-item-cover .like-icon').click(function() {
+		var _thisJq = $(this);
+		if (_thisJq.hasClass('do-like')) {
+			_thisJq.removeClass('do-like').addClass('donot-like');
+		} else if (_thisJq.hasClass('donot-like')) {
+			_thisJq.removeClass('donot-like').addClass('do-like');
+		}
 	})
+
 })
