8851788dfcbfd90b718d596a41de99dd02d3fe7b
xunke <frequdem@163.com>
2016-12-14 00:40:50 +0800

    asdf

--- routes/list.js
+++ routes/list.js
@@ -15,46 +15,14 @@ router.get('/list', function(req, res, next) {
 	var data;
 	var lgSt = checkSession(req);
 
-	Promise.all([new Promise(function(resolve, reject){
+
 		House.find({}).lean().exec( function(err, data) {
 			if (err) {
 				reject(err);
 			} else {
-				resolve(data);
+				res.render('list/list', {list: data, logStatus: lgSt});
 			}		
 		})
-	}), new Promise(function(resolve, reject) {
-		if (lgSt) {
-			User.findOne({_id: req.session.userId}, function(err, data) {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(data);
-				}	
-			})
-		} else {			
-			resolve();
-
-		}
-	})]).then(function(datas) {
-		data = datas[0]; 
-		
-		if (!!datas[1] && datas[1].like.length) {
-
-			datas[1].like.map(function(item) {
-				data.forEach(function(value, index, arr) {
-					if (item === value._id.toString()) {						
-						data[index].like = true;
-					}					
-				})
-			})
-		}
-		res.render('list/list', {list: data, logStatus: lgSt});
-	}).catch(function(err) {
-		console.log(err);
-	});
-		
-
 })
 
 module.exports = router;
